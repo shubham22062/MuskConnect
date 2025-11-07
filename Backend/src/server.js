64885a197249c7b2f6f6af53f3eacc7c14967516
Shubham Awasthi <shubhamawasthi021@gmail.com>
2025-11-07 22:57:58 +0530

    sever

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,7 @@ import "dotenv/config"
 import authRoutes from "./routes/auth.routes.js"
 import { connect } from "mongoose";
 import { connectDB } from "./lib/db.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
