cff7f475fb5523d704eb41b508a812b7181c4a8b
Shubham Awasthi <shubhamawasthi021@gmail.com>
2025-11-16 20:39:13 +0530

    authRoute

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -12,8 +12,25 @@ export const protectRoute = async (req,res,next)=>{
         }
         
 
-        const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY)
+
+        if(!decoded) {
+            return res.status(401).json({message:"Unauthorized-Invalid token"});
+
+        }
+
+        const user = await User.findById(decoded.userId).select("-password")
+
+        if(!user){
+            return res.status(401).json({message:"Unauthorized - User not found"})
+
+        }
+
+        req.user= user;
+        next();
     } catch (error) {
+        console.log("Error in protectRoute middleware",error);
+        res.status(500).json({message:"Internal Server Error"});
         
     }
 }
