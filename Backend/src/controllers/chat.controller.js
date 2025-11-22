494a7436b79bd793b1b374db8e6baabd13388b79
Shubham Awasthi <shubhamawasthi021@gmail.com>
2025-11-22 22:29:46 +0530

    streamController

---
+++ Backend/src/controllers/chat.controller.js
@@ -0,0 +1,11 @@
+import { generateStreamToken } from "../lib/stream.js";
+
+export async function generateStreamToken(req,res){
+    try {
+        const token = generateStreamToken(req.user.id);
+        res.status(200).json({token})
+    } catch (error) {
+        console.log("Error in getStreamToken Controller:", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
